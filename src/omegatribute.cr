a87fcd4aab8f68ddf697987930c16b26711c6c85
Hirofumi Wakasugi <baenej@gmail.com>
2015-12-05 21:55:40 +0900

    Add feature to copy markdown files from repo to omegat

--- src/omegatribute.cr
+++ src/omegatribute.cr
@@ -2,6 +2,21 @@ require "./omegatribute/*"
 
 module Omegatribute
 
+  def self.copy_to_omegat_source_directory(src_dir, dst_dir)
+    Dir.glob(src_dir + "/*") do |f|
+      if File.directory?(f)
+        self.copy_to_omegat_source_directory(f, File.join(dst_dir, File.basename(f)))
+      end
+      next unless File.extname(f) == ".md"
+      dst_file = File.join(dst_dir, File.basename(f))
+      puts "FROM: #{f}"
+      puts "TO: #{dst_file}"
+      Dir.mkdir_p(File.dirname(dst_file))
+      File.write(dst_file, File.read(f))
+    end
+  end
+
+
   def self.copy_back_md_to_repo(src_dir, dst_dir)
     Dir.glob(src_dir + "/*") do |f|
       if File.directory?(f)
