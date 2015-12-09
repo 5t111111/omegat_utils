cf45d1d00cac79af493d6a12e3b4822687a1eb4d
Hirofumi Wakasugi <baenej@gmail.com>
2015-12-10 01:15:16 +0900

    Refator methods and variables name

--- src/omegatribute.cr
+++ src/omegatribute.cr
@@ -4,22 +4,16 @@ require "crypto/md5"
 
 module Omegatribute
 
-  def self.compare_files(file_a, file_b)
-    file_a_md5 = Crypto::MD5.hex_digest(File.read(file_a))
-    file_b_md5 = Crypto::MD5.hex_digest(File.read(file_b))
-    file_a_md5 == file_b_md5
-  end
-
-  def self.copy_to_omegat_source_directory(src_dir, dst_dir)
-    Dir.glob(src_dir + "/*") do |f|
+  def self.copy_from_repo_to_omegat_source(repo_dir, source_dir)
+    Dir.glob(repo_dir + "/*") do |f|
       if File.directory?(f)
-        self.copy_to_omegat_source_directory(f, File.join(dst_dir, File.basename(f)))
+        self.copy_from_repo_to_omegat_source(f, File.join(source_dir, File.basename(f)))
       end
       next unless File.extname(f) == ".md"
-      dst_file = File.join(dst_dir, File.basename(f))
+      dst_file = File.join(source_dir, File.basename(f))
 
       if File.exists?(dst_file)
-        next if compare_files(f, dst_file)
+        next if compare_two_files(f, dst_file)
       else
         puts "NEW:  #{dst_file}".colorize(:yellow)
       end
@@ -30,17 +24,23 @@ module Omegatribute
     end
   end
 
-
-  def self.copy_back_md_to_repo(src_dir, dst_dir)
-    Dir.glob(src_dir + "/*") do |f|
+  def self.copy_back_from_omegat_target_to_repo(target_dir, repo_dir)
+    Dir.glob(target_dir + "/*") do |f|
       if File.directory?(f)
-        self.copy_back_md_to_repo(f, File.join(dst_dir, File.basename(f)))
+        self.copy_back_from_omegat_target_to_repo(f, File.join(repo_dir, File.basename(f)))
       end
       next unless File.extname(f) == ".md"
-      dst_file = File.join(dst_dir, File.basename(f))
+      dst_file = File.join(repo_dir, File.basename(f))
       puts "FROM: #{f}".colorize(:cyan)
       puts "TO:   #{dst_file}".colorize(:magenta)
       File.write(dst_file, File.read(f))
     end
   end
+
+  private def self.compare_two_files(file_a, file_b)
+    file_a_md5 = Crypto::MD5.hex_digest(File.read(file_a))
+    file_b_md5 = Crypto::MD5.hex_digest(File.read(file_b))
+    file_a_md5 == file_b_md5
+  end
+
 end
